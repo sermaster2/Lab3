3da0a1654e332a47c299aeea8fe56430924fc69b
sermaster2 <sec106@pitt.edu>
2015-09-27 18:02:48 -0400

    Fixed Lab3b

--- Lab3b.java
+++ Lab3b.java
@@ -9,14 +9,16 @@ public class Lab3b {
 	Scanner sc = new Scanner(System.in);
 
 	System.out.print("Enter number of bottles of root beer on wall > ");
+	int bottles;
 
-	if (sc.hasNextInt()) {
-		int bottles = sc.nextInt();
-		}
+	if (sc.hasNextInt()) {}
 	else {
 		System.out.println("You typed something other than an integer!");
 		String somethingElse = sc.next();
+		System.exit(0);
 		}	
+
+	bottles = sc.nextInt();
 
 		if (bottles <= 0 || bottles > 99) {
 		    System.out.println("Number must be > 0 and < 100 !");
@@ -24,15 +26,14 @@ public class Lab3b {
 
 		else {
 
-		for (int a = 1; a <= bottles; a++) {
-			for (int b = bottles; b >= a; b--) {
-				if (b >= a)
-				System.out.print("R");
+			for (int a = 1; a <= bottles; a++) {
+				for (int b = bottles; b >= a; b--) {
+					if (b >= a)
+					System.out.print("R");
+				}
+			System.out.println();
 			}
-		    System.out.println();
-		}
 		System.out.println("NO MORE BOTTLES OF ROOT BEER ON THE WALL!");
-
 			 
 		}
 
